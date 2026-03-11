a80622f7c9427fa1dc83115d4730c6c28c052dd5
00edgar <shyzedgar@gmai.com>
2026-03-11 08:14:44 -0600

    feat: :rocket: agregar
    funcion agregar y guardar

--- main.py
+++ main.py
@@ -16,6 +16,33 @@ def menu():
           3: para eliminar servicio
           4: para salir
           """)
+    
+def guardar(datos):
+    with open("datos.json", "" )as registro: 
+        json.dump(datos ,registro ,indent=4  )
+        
+        
+    
+def agregar_servicio():
+    id_servicio= input ("ingrese identificador")
+    servicio= input ("ingrese nombre del el servicio")
+    definicion= input ("ingrese una descripcion del producto")
+    precio= float (input("ingrese el costo del servicio"))
+    
+    nuevo_servicio={
+        "id_servicio",id_servicio,
+        "servicio",servicio,
+        "definicion",definicion,
+        "precio",precio
+    }
+    datos.apened(nuevo_servicio)
+    guardar(datos) 
+    print ("agregacion de srvicio completada")
+    
+    
 menu()
 separador()
-    
+while True:
+    opc=int (input("ingrese su opcion deseada"))
+    if opc==1:
+        agregar_servicio()
