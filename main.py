0bafadf94973f87a2074a0122669223910f54c16
00edgar <shyzedgar@gmai.com>
2026-03-11 09:21:39 -0600

    feat: :rocket: eliminar
    permite eliminar un servicio

--- main.py
+++ main.py
@@ -16,6 +16,64 @@ def menu():
           3: para eliminar servicio
           4: para salir
           """)
+    
+def guardar(datos):
+    with open("datos.json", "" )as registro: 
+        json.dump(datos ,registro ,indent=4  )
+        
+        
+    
+def agregar_servicio(datos):
+
+    servicio= input ("ingrese nombre del el servicio")
+    definicion= input ("ingrese una descripcion del producto")
+    precio= float (input("ingrese el costo del servicio"))
+    
+    nuevo_servicio={  
+        "id_servicio": len(datos) + 1,
+        "servicio":servicio,
+        "definicion":definicion,
+        "precio":precio
+    }
+    datos.apened(nuevo_servicio)
+    guardar(datos) 
+    print ("agregacion de srvicio completada")
+    
+def editar_servicio(datos, campo):
+    id_buscar = int(input("Ingrese el ID del elemento a editar: "))
+
+    for elemento in datos:
+        if elemento["id servicio"] == id_buscar:
+            nuevo_valor = input("Ingrese el nuevo valor: ")
+            elemento[campo] = nuevo_valor
+            guardar(datos)
+            print("Elemento actualizado correctamente.\n")
+            return
+
+    print("Elemento no encontrado.\n")
+
+def eliminar_servicio(datos):
+    titulo = input("Ingrese el título a eliminar: ")
+
+    for elemento in datos:
+        if elemento["nombre"].lower() == titulo.lower():
+            datos.remove(elemento)
+            guardar(datos)
+            print("Elemento eliminado correctamente.\n")
+            return
+
+    print("Elemento no encontrado.\n")
+
+
+    
 menu()
 separador()
+while True:
+    opc=int (input("ingrese su opcion deseada"))
+    if opc==1:
+        agregar_servicio()
+    elif opc==2:
+        editar_servicio()
+    elif opc==3 :
+        eliminar_servicio()
     
